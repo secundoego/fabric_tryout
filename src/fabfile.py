a85973658b0e51f3cc4520d2191daa3d5d8707c4
secundoego <secundoego@zoho.com>
2014-06-13 21:08:59 +0200

    Do commit

--- src/fabfile.py
+++ src/fabfile.py
@@ -10,5 +10,17 @@ def hello(name):
 def test():
 	local("ls -al")
 	
+# Add and local changes to local repository
+def add():
+	local("git add -p")
+	
 def commit():
-	local("git add -p && git commit")
+	local("git commit")
+
+# Push local changes to remote GitHub
+def push():
+	local("git push fabric_tryout")
+
+# Pull remote repository from GitHub to local system	
+def pull():
+	local("git pull fabric_tryout master")
