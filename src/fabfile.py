a0d5652ea99c522d874fd742d928390ff69b3b28
secundoego <secundoego@zoho.com>
2014-06-13 21:59:38 +0200

    moved file

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,4 +1,10 @@
-from fabric.api import local
+from __future__ import with_statement # to use Python's with:
+from fabric.api import local 
+from fabric.api import settings, abort
+from fabric.contrib.console import confirm
+from fabric.operations import run
+from fabric.operations import put
+from fabric.context_managers import cd
 
 # Call code: 
 #	>fab hello:name=Koen
@@ -24,3 +30,13 @@ def push():
 # Pull remote repository from GitHub to local system	
 def pull():
 	local("git pull fabric_tryout master")
+	
+def deploy():
+	code_dir = '/src'
+	with cd(code_dir):
+		#local("ls -al")
+		run("git pull fabric_tryout master")
+		#run("touch app.wsgi")
+
+def deploy_remote():
+	fabric.operations.put("/src/*", remote_path, use_sudo=False, mirror_local_mode=False, mode=None)
