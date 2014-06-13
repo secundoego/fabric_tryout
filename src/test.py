c75f8150f16d7a129ebab01403a21537fdfc4a0f
secundoego <secundoego@zoho.com>
2014-06-13 21:18:23 +0200

    test2 created

---
+++ src/test.py
@@ -0,0 +1,15 @@
+'''
+test
+
+<what it does>
+
+Input:
+Output:
+
+@author: Koen Warner (koen)
+@version:  v0.1,  13 jun. 2014
+(c) 2014 Koen Warner
+'''
+
+def hello(name): 
+	print("Hello %s" % name)
