a2728c4e326c9621d5e610214a6b6ade479cc5d5
secundoego <secundoego@zoho.com>
2014-06-14 15:43:39 +0200

    changed fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,16 +2,42 @@ from fabric.api import local, env
 from fabric.operations import run, put
 from fabric.context_managers import cd
 
-# When using an argument for a fabfile 
-#	>fab hello:name=Koen
-#	>fab hello:Koen
-# name is arg to function hello()
-def hello(name): 
-	print("Hello %s" % name)
+'''
+fabfile.py
+
+Functions
+- Git local and remote actions
+- Deploy application to remote server
+
+Input:
+Output: 
+
+@author: Koen Warner (koen)
+@version:  v0.1,  14 jun. 2014
+(c) 2014 Koen Warner
+'''
+
+####################
+# Global variables #
+####################
+
+project_name ="fabric_tryout"
+
+remote_user = "pi"
+# remote_host = "10.0.0.18"
+# remote_host = "192.168.2.4"
+remote_host = "81.68.58.163"
+remote_port = "2222"
+#remote_port = "2226"
+local_path = "./src/*"
+remote_path = "~/fabric_tryout/"
+env.hosts = ["%s@%s:%s" % (remote_user,  remote_host, remote_port)]
+
 
 ################
 # Git commands #
 ################
+
 
 # Add all local changes to local repository
 #	-all: also handles removals
@@ -24,25 +50,22 @@ def commit():
 
 # Push local changes to remote GitHub
 def push():
-	local("git push fabric_tryout")
+	print "Pushing %s to GitHub" % project_name
+	local("git push %s" % project_name)
 
 # Pull remote repository from GitHub to local system	
 def pull():
-	local("git pull fabric_tryout master")
+	local("git pull %s" % project_name)
 
 
 ###################
 # Deploy commands #
 ###################
 	
-# Deploy application to remote server
-# 
 
-# user server ssh-port to deploy to
-env.hosts = ['pi@10.0.0.18:2222']
-
-# Deploy from local_path to remote_path
-def deploy_remote():
-	put("./src/*", "~/fabric_tryout/")
+# Deploy from local to remote server
+# NB: the problem with put is that locally removed files are not removed remotely.
+def deploy():
+	put("%s" % local_path, "%s" % remote_path)
 	
 	
