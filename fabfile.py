0fa13e733ed8b5b030a99409d1bc9d370b54b522
secundoego <secundoego@zoho.com>
2014-06-16 17:45:16 +0200

    changes

--- fabfile.py
+++ fabfile.py
@@ -25,13 +25,11 @@ Output:
 project_name ="fabric_tryout"
 
 remote_user = "pi"
-remote_host = "10.0.0.18"
-# remote_host = "192.168.2.4"
-#  	remote_host = "81.68.58.163"
+#remote_host = "10.0.0.18"
+#remote_host = "192.168.2.4"
+remote_host = "81.68.58.163"
 remote_port = "2222"
 #remote_port = "2226"
-local_path = "./src/*"
-remote_path = "~/fabric_tryout/"
 env.hosts = ["%s@%s:%s" % (remote_user,  remote_host, remote_port)]
 
 
@@ -66,8 +64,9 @@ def pull():
 
 # Deploy from local to remote server
 
-# fabric.contrib.project.rsync_project source definition
-#	[https://github.com/fabric/fabric/blob/master/fabric/contrib/project.py} 
+# We're using: fabric.contrib.project.rsync_project 
+#
+# source definition [https://github.com/fabric/fabric/blob/master/fabric/contrib/project.py} 
 #
 #	def rsync_project(
 #    remote_dir,
@@ -80,8 +79,8 @@ def pull():
 #    upload=True,
 #    default_opts='-pthrvz'
 #	):
-
-# The rsync_project() below will give
+#
+# The usage of rsync_project() below will give the following rsync command
 #	rsync --delete -pthrvz -EgolHDu --out-format="info - %i %B %12b %12l %M %f%L" --rsh='ssh  -p 2222 ' src/ pi@10.0.0.18:~/fabric_tryout
 #		rsync					generate by fabric 
 #		--delete 				added by programmer as: delete=True
@@ -91,10 +90,27 @@ def pull():
 #		src/ 					added by programmer
 #		pi@10.0.0.18:			added by Fabric using variable env.hosts
 #		~/fabric_tryout			added by programmer
+
 def deploy():
-	local= "src/"
-	remote = "~/fabric_tryout"
+	# Everything in local_path(exluding the directory itself) is copied to remoter_path directory
+	# mind the trailing slash
+	local_path= "src/"
+	# mind the missing trailing slash
+	remote_path = "~/fabric_tryout"
+	# rsync options added by programmer
 	opt = '-EgolHDu --out-format="info - %i %B %12b %12l %M %f%L"'
-	rsync_project(remote, local, delete=True, extra_opts=opt)
+	
+	print "==> Deploying application using the following parameters"
+	print "\tremote host\t\t %s" % remote_host
+	print "\tremote port\t\t %s" % remote_port
+	print "\tremote user\t\t %s" % remote_user
+	print "t\tlocal directory\t\t %s" % local_path
+	print "\tremote directory\t %s" % remote_path
+	
+	answer = raw_input("Continue [y/n]?")
+	if (answer == "y"):
+		rsync_project(remote_path, local_path, delete=True, extra_opts=opt)
+	else: 
+		print "Aborted"
 	
 	
